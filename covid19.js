40c74ba79c7ded97378d7b45c6ebe94812ec6a4f
Samuel Cardillo <iam@samuelcardillo.com>
2020-04-17 21:50:50 +0300

    Added more infos to parse

--- covid19.js
+++ covid19.js
@@ -66,45 +66,55 @@ function compare(genSeqOne, genSeqTwo) {
   console.log(`${diff.total} possible diff in compared sequences.`);
 }
 
-// downloadGenome("MN938384");
-generateReport("MN938384");
-
 // Download a genome from the NCBI GenBank
 function downloadGenome(name) {
-  request(`https://www.ncbi.nlm.nih.gov/nuccore/${name}`, async (err, res) => {
-    let uid = res.body.split("ordinalpos=1&amp;ncbi_uid=");
-    uid = uid[1].split('&')[0];
-    let link = `https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=${uid}&db=nuccore&report=genbank&conwithfeat=on&withparts=on&hide-cdd=on&retmode=html&withmarkup=on&tool=portal&log$=seqview&maxdownloadsize=1000000`
-    
-    await request(link, async (err, res) => {
-      let features = res.body.split(/(?:_CDS_[0-9]" class="feature">)/);
-      let fullSequence = [];
-      let fullGenome = {
-        proteins: [],
-        code: null
-      }
+  return new Promise((resolve, reject) => {
+    request(`https://www.ncbi.nlm.nih.gov/nuccore/${name}`, (err, res) => {
+      let uid = res.body.split("ordinalpos=1&amp;ncbi_uid=");
+      uid = uid[1].split('&')[0];
+      let link = `https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=${uid}&db=nuccore&report=genbank&conwithfeat=on&withparts=on&hide-cdd=on&retmode=html&withmarkup=on&tool=portal&log$=seqview&maxdownloadsize=1000000`
+      
+      request(link, (err, res) => {
+        let features = res.body.split(/(?:_CDS_[0-9]" class="feature">)/);
+        let genomeInfos = res.body.split("_source_0\" class=\"feature\">")[1];
+        let fullSequence = [];
+        let fullGenome = {
+          infos: {
+            name: genomeInfos.split("/organism=\"")[1].split("\"")[0],
+            date: genomeInfos.split("/collection_date=\"")[1].split("\"")[0],
+            isolate: genomeInfos.split("/isolate=\"")[1].split("\"")[0],
+            country: genomeInfos.split("/country=\"")[1].split("\"")[0],
+            host: genomeInfos.split("/host=\"")[1].split("\"")[0],        
+          },
+          proteins: [],
+          code: null
+        }
 
-      // Start at 1 -- 0 is overhead
-      for(var i = 1; i < features.length; i++) {
-        let name = features[i].split("/gene=\"")[1].split("\"")[0];
-        let sequence = JSON.parse(`[${features[i].split("features[\"CDS\"].push([")[1].split("]);")[0]}]`);
-        let start = sequence[0][0];
-        let end = (sequence.length > 1) ? sequence[1][1] : sequence[0][1];
+        // Start at 1 -- 0 is overhead
+        for(var i = 1; i < features.length; i++) {
+          let name = features[i].split("/gene=\"")[1].split("\"")[0];
+          let sequence = JSON.parse(`[${features[i].split("features[\"CDS\"].push([")[1].split("]);")[0]}]`);
+          let start = sequence[0][0];
+          let end = (sequence.length > 1) ? sequence[1][1] : sequence[0][1];
 
-        fullGenome.proteins.push({
-          name: name,
-          start: start,
-          end: end
+          fullGenome.proteins.push({
+            name: name,
+            start: start,
+            end: end
+          })
+        }
+
+        let unparsedSequences = res.body.split(/(?:<span class="ff_line" id="[A-Z]{1,}[0-9]{1,20}(?:.)[0-9]{1,10}(?:_)[0-9]{1,10}">)/);
+        for(var i2 = 1; i2 < unparsedSequences.length; i2++) { 
+          fullSequence.push(unparsedSequences[i2].split("</span>")[0]);
+        }
+        fullGenome.code = fullSequence.join('').split(' ').join('');
+
+        fs.writeFile(`./${name}.json`, JSON.stringify(fullGenome), (err) => {
+          if(err) return reject();
+          return resolve();
         })
-      }
-
-      let unparsedSequences = res.body.split(/(?:<span class="ff_line" id="[A-Z]{1,}[0-9]{1,20}(?:.)[0-9]{1,10}(?:_)[0-9]{1,10}">)/);
-      for(var i2 = 1; i2 < unparsedSequences.length; i2++) { 
-        fullSequence.push(unparsedSequences[i2].split("</span>")[0]);
-      }
-      fullGenome.code = fullSequence.join('').split(' ').join('');
-
-      fs.writeFile(`./${name}.json`, JSON.stringify(fullGenome), (err) => {})
+      })
     })
   })
 }
@@ -121,3 +131,11 @@ function generateReport(name) {
     }
   })
 }
+
+
+/////// USAGE -- Temporary
+
+let name = "MN938384";
+downloadGenome(name).then(() => {
+  generateReport(name);
+})
